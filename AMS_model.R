2cd18710377459d5655ed052e421dbb143230f8f
Viktoria Szabo <v.szabo@hotmail.de>
2022-02-19 11:21:22 +0100

    Both ORs

--- AMS_model.R
+++ AMS_model.R
@@ -137,6 +137,8 @@ table(mighint12g_new)
 table(r_besch)
 data$EMPLOYMENTDAYS <- factor(r_besch, levels = c(0, 1), labels = c("<90 Tage", ">=90 Tage"), ordered = FALSE)
 table(data$EMPLOYMENTDAYS)
+data$truth <- ifelse(data$EMPLOYMENTDAYS == ">=90 Tage", 1, 0)
+table(data$truth)
 
 # Save dataset =================================================================
 saveRDS(data, "data/JuSAW_prepared.rds")
@@ -175,18 +177,16 @@ optimal <- optimalCutoff(data$truth, data$prob_ams, optimiseFor = "misclasserror
 # Confusion matrix
 confusionMatrix(data$class_pred, data$truth)
 
-#calculate sensitivity
+#calculate sensitivity, specificity, total misclassification error rate
 sensitivity(data$truth, data$class_pred)
-#calculate specificity
 specificity(data$truth, data$class_pred)
-#calculate total misclassification error rate
 misClassError(data$truth, data$class_pred, threshold=0.66)
 
 # Checking ROC
 library(Epi)
 ROC(form = EMPLOYMENTDAYS ~ GENDER_female + AGEGROUP + STATEGROUP 
     + EDUCATION + CHILDCARE + RGS
-    + IMPAIRMENT + OCCUPATIONGROUP + BESCHÄFTIGUNGSVERLAUF
+    + IMPAIRMENT + OCCUPATIONGROUP + EMPLOYMENT
     + BUSINESSCASEDUR + BUSINESSCASEFREQ + SUPPORTMEASURE, data=data,plot="ROC")
 
 # Looking at variable effects
@@ -206,7 +206,7 @@ plot(rstudent(model1), type='l')
 # Logistic regression model with "better" AMS variables
 model_alt <- glm(EMPLOYMENTDAYS ~ GENDER_female + AGEGROUP + STATEGROUP 
                  + EDUCATION + CHILDCARE_both + RGS
-                 + IMPAIRMENT + OCCUPATIONGROUP_all + BESCHÄFTIGUNGSVERLAUF
+                 + IMPAIRMENT + OCCUPATIONGROUP_all + EMPLOYMENT
                  + GESCHÄFTSFALLDAUER + BUSINESSCASEFREQ + SUPPORTMEASURE, 
                  family = "binomial", data = data)
 summary(model1)
@@ -217,25 +217,36 @@ predicted_prob_alt <- predict(model1, data, type="response")
 
 # Odds Ratios from AMS documentation ###########################################
 # Kurzfristiges Kriterium: Partiell valide schätzbare jugendliche Population
-model_ams_OR <- glm(EMPLOYMENTDAYS ~ GENDER_female + AGEGROUP 
+model_ams_OR_youth <- glm(EMPLOYMENTDAYS ~ GENDER_female + AGEGROUP 
                  + EDUCATION + CHILDCARE + RGS
                  + IMPAIRMENT + OCCUPATIONGROUP 
                  + BUSINESSCASEFREQ + SUPPORTMEASURE, 
                  family = "binomial", data = data)
-summary(model_ams_OR)
+summary(model_ams_OR_youth)
 OR_coefs_youth <- c(-0.13, 0.09, 0.13, 0.0, 0.48, 0.40, -0.46, -0.10, -0.17, -0.15, 
               -0.36, -0.03, 0.01, -0.02, -0.06, -0.21, -0.14)
-model_ams_OR$coefficients <- OR_coefs_youth
+model_ams_OR_youth$coefficients <- OR_coefs_youth
+
+# Predictions 
+data$prob_ams_OR_youth <- predict(model_ams_OR_youth, data, type="response")
+
+data$class_pred_OR_youth <-ifelse(data$prob_ams_OR_youth > 0.66, 1, 0)
+table(data$class_pred_OR_youth)
+
+
+# Kurzfristiges Kriterium: Voll valide shcätzbare Population
+model_ams_OR <- glm(EMPLOYMENTDAYS ~ GENDER_female + AGEGROUP + STATEGROUP
+                    + EDUCATION + CHILDCARE + RGS + IMPAIRMENT 
+                    + OCCUPATIONGROUP + EMPLOYMENT
+                    + BUSINESSCASEFREQ + BUSINESSCASEDUR + SUPPORTMEASURE, 
+                    family = "binomial", data = data)
+summary(model_ams_OR)
+OR_coefs <- c(0.48, -0.05, 0.0, 0.0, -0.01, 0.04, 0.10, 0.03, -0.08, -0.10, 
+              -0.16, -0.10, -0.33, 0.07, -0.31, 0.20, 0.36, 0.63, -0.21, -0.19, -0.03)
+model_ams_OR$coefficients <- OR_coefs
 
 # Predictions 
 data$prob_ams_OR <- predict(model_ams_OR, data, type="response")
 
 data$class_pred_OR <-ifelse(data$prob_ams_OR > 0.66, 1, 0)
 table(data$class_pred_OR)
-data$truth <- ifelse(data$EMPLOYMENTDAYS == ">=90 Tage", 1, 0)
-table(data$truth)
-
-# Confusion matrix
-confusionMatrix(data$class_pred, data$truth)
-
-
