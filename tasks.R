13437363bda3e2a294028d02f109093201907093
Viktoria Szabo <40633060+vik-sz@users.noreply.github.com>
2022-05-22 12:44:11 +0200

    set equal testsets as holdout in row_roles

--- tasks.R
+++ tasks.R
@@ -9,6 +9,10 @@ data <- readRDS("data/JuSAW_prepared.rds")
 # Load other scripts ---------------------------------------------------------------------------------------------------
 source("variable_sets.R", encoding="utf-8") # for predefined feature sets
 
+# Load test ids
+#write(ids, file = "test_ids.txt")
+test_ids = scan("test_ids.txt")
+
 # Task set-up ==========================================================================================================
 # Set up different tasks with different variable sets 
 # AMS Taks -------------------------------------------------------------------------------------------------------------
@@ -17,17 +21,26 @@ task_ams_youth = as_task_classif(data[ams_youth], target = "EMPLOYMENTDAYS", pos
 #task_ams_youth = as_task_classif(data[ams_youth], target = "r_besch", positive = 1, id = "AMS youth")
 
 #task_ams_youth$set_col_roles("case", roles = "name")
+# Set test set holdout rows
+task_ams_youth$set_row_roles(test_ids, roles = "holdout")
 
+# Set protected attribute
+task_ams_youth$col_roles$pta = "GENDER"
+
+# remove incomplete rows
 ids = complete.cases(task_ams_youth$data())
 sum(!ids)
 task_ams_youth$filter(which(ids))
 
-# Set protected attribute
-task_ams_youth$col_roles$pta = "GENDER"
 print(task_ams_youth) # 950 Beobachtungen mit 10 Variablen
+
 
 # Original AMS full ____________________________________________________________________________________________________
 task_ams = as_task_classif(data[ams], target = "EMPLOYMENTDAYS", positive = ">=90 Days", id = "AMS")
+
+# Set test set holdout rows
+task_ams$set_row_roles(test_ids, roles = "holdout")
+
 ids = complete.cases(task_ams$data())
 sum(!ids)
 task_ams$filter(which(ids))
@@ -38,6 +51,10 @@ print(task_ams) # 950 Beobachtungen mit 10 Variablen
 
 # AMS extended _________________________________________________________________________________________________________
 task_ams_ext = as_task_classif(data[ams_ext], target = "EMPLOYMENTDAYS", positive = ">=90 Days", id = "AMS extended")
+
+# Set test set holdout rows
+task_ams_ext$set_row_roles(test_ids, roles = "holdout")
+
 ids = complete.cases(task_ams_ext$data())
 sum(!ids)
 task_ams_ext$filter(which(ids))
@@ -51,6 +68,10 @@ unique(task_ams_ext$feature_types$type)
 # Explorative Analysis variables ---------------------------------------------------------------------------------------
 # Green big ____________________________________________________________________________________________________________
 task_green_big = as_task_classif(data[green_big], target = "EMPLOYMENTDAYS", positive = ">=90 Days", id = "green big")
+
+# Set test set holdout rows
+task_green_big$set_row_roles(test_ids, roles = "holdout")
+
 ids = complete.cases(task_green_big$data())
 sum(!ids)
 task_green_big$filter(which(ids))
@@ -64,6 +85,9 @@ unique(task_green_big$feature_types$type)
 # Green small __________________________________________________________________________________________________________
 task_green = as_task_classif(data[green], target = "EMPLOYMENTDAYS", positive = ">=90 Days", id = "green")
 
+# Set test set holdout rows
+task_green$set_row_roles(test_ids, roles = "holdout")
+
 ids = complete.cases(task_green$data())
 sum(!ids)
 task_green$filter(which(ids))
@@ -73,3 +97,7 @@ print(task_green) # 743 Beobachtungen mit 30 Variablen
 
 
 unique(task_green$feature_types$type)
+
+# Check test set equality ==============================================================================================
+setequal(task_ams_youth$row_roles$holdout, task_green$row_roles$holdout)
+
