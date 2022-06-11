8eb71602f7337ca7b26d63bbb17c575556732f81
Viktoria Szabo <40633060+vik-sz@users.noreply.github.com>
2022-06-11 09:32:59 +0200

    Evalutation for all tasks

--- evaluation.R
+++ evaluation.R
@@ -21,60 +21,66 @@ source("functions.R", encoding="utf-8") # for predefined feature sets
 
 # Performance ==========================================================================================================
 # Confucion Matrix -----------------------------------------------------------------------------------------------------
-conf_mats = subset(df1, subset = (task == "AMS youth")) %>%
-  group_by(model) %>%
-  conf_mat(truth_01, estimate = estimate_0.66)
+# 0.66 Threshold
+confmat_list_0.66 = list()
+for(i in unique(df1$task)){
+  conf_mats_0.66 = subset(df1, subset = (task == i)) %>%
+    group_by(model) %>%
+    conf_mat(truth_01, estimate = estimate_0.66)
+  
+  names(conf_mats_0.66$conf_mat) = conf_mats_0.66$model
+  
+  confmat_list_0.66 = append(confmat_list_0.66, list(conf_mats_0.66))
+  #conf_mats_0.66$conf_mat
+}
+names(confmat_list_0.66) = unique(df1$task)
 
-names(conf_mats$conf_mat) = conf_mats$model
-conf_mats$conf_mat
+# 0.5 Threshold
+confmat_list_0.5 = list()
+for(i in unique(df1$task)){
+  conf_mats_0.5 = subset(df1, subset = (task == i)) %>%
+    group_by(model) %>%
+    conf_mat(truth_01, estimate = estimate_0.5)
+  
+  names(conf_mats_0.5$conf_mat) = conf_mats_0.5$model
+  
+  confmat_list_0.5 = append(confmat_list_0.5, list(conf_mats_0.5))
+  #conf_mats_0.5$conf_mat
+}
+names(confmat_list_0.5) = unique(df1$task)
 
-conf_mats_0.5 = subset(df1, subset = (task == "AMS youth")) %>%
-  group_by(model) %>%
-  conf_mat(truth_01, estimate = estimate_0.5)
-
-names(conf_mats_0.5$conf_mat) = conf_mats_0.5$model
-conf_mats_0.5$conf_mat
 
 # ROC and PRC Curves ---------------------------------------------------------------------------------------------------
+roc_list = list()
 for(i in unique(df1$task)){
-  subset(df1, subset = (task == i)) %>%
-    filter(!is.na(probabilities)) %>%
+  sub = subset(df1, subset = (task == i)) %>%
+    filter(!is.na(probabilities)) %>% # since not all tasks have OR results
     #filter(model == "OR" | model == "Logistic Regression" | model == "encode.colapply.classif.glmnet") %>%
-    group_by(model) %>%
-    roc_curve(truth_01, probabilities, event_level = 'second') %>%
-    autoplot() +
+    group_by(model)
+  
+   roc_plot = autoplot(roc_curve(sub, truth_01, probabilities, event_level = 'second'))  +
     ggtitle(i)
+   
+   roc_list = append(roc_list, list(roc_plot))
 }
 
+# PRC curve
+prc_list = list()
 for(i in unique(df1$task)){
   sub = subset(df1, subset = (task == i)) %>%
     filter(!is.na(probabilities)) %>%
     #filter(model == "OR" | model == "Logistic Regression" | model == "encode.colapply.classif.glmnet") %>%
     group_by(model)
   
-   autoplot(roc_curve(sub, truth_01, probabilities, event_level = 'second'))  +
+  prc_plot = autoplot(pr_curve(sub, truth_01, probabilities, event_level = 'second')) +
     ggtitle(i)
+  
+  prc_list = append(prc_list, list(prc_plot))
+  
 }
 
-subset(df1, subset = (task == "AMS youth")) %>%
-  #filter(model == "OR" | model == "Logistic Regression" | model == "encode.colapply.classif.glmnet") %>%
-  group_by(model) %>%
-  roc_curve(truth_01, probabilities, event_level = 'second') %>%
-  autoplot() +
-  ggtitle("AMS youth")
 
-
-# PRC curve
-subset(df1, subset = (task == "AMS youth")) %>%
-  #filter(model == "OR" | model == "Logistic Regression" | model == "encode.colapply.classif.glmnet") %>%
-  group_by(model) %>%
-  pr_curve(truth_01, probabilities, event_level = 'second') %>%
-  autoplot() +
-  ggtitle("AMS youth")
-
-
-# Performance Measures and Heatmap -------------------------------------------------------------------------------------
-# Measures
+# Measures -------------------------------------------------------------------------------------------------------------
 performance_measure_set = metric_set(accuracy, 
                                      sens, # sensitivity, recall, TPR
                                      spec, # specificity, TNR
@@ -82,55 +88,105 @@ performance_measure_set = metric_set(accuracy,
                                      f_meas # fbeta, with beta = 1
                                      )
 
+measure_list = list()
+for(i in unique(df1$task)){
+  performance_measures = df1 %>%
+    filter(task == i) %>%
+    filter(!is.na(estimate_0.66)) %>%
+    group_by(model) %>%
+    performance_measure_set(truth_01, estimate = estimate_0.66, event_level = 'second')
+  
+  measures_female = df1 %>%
+    filter(task == i & GENDER == "female") %>%
+    filter(!is.na(estimate_0.66)) %>%
+    group_by(model) %>%
+    performance_measure_set(truth_01, estimate = estimate_0.66, event_level = 'second') %>%
+    rename(female = .estimate)
+  
+  measures_male = df1 %>%
+    filter(task == i & GENDER == "male") %>%
+    filter(!is.na(estimate_0.66)) %>%
+    group_by(model) %>%
+    performance_measure_set(truth_01, estimate = estimate_0.66, event_level = 'second') %>%
+    rename(male = .estimate)
+  
+  measures = full_join(performance_measures, measures_female, by = c("model", ".metric", ".estimator"))
+  measures = full_join(measures, measures_male, by = c("model", ".metric", ".estimator"))
+  
+  # AUC
+  auc = df1 %>%
+    filter(task == i & GENDER == "female") %>%
+    filter(!is.na(probabilities)) %>%
+    group_by(model) %>%
+    roc_auc(truth_01, probabilities, event_level = 'second')
+  
+  auc_female = df1 %>%
+    filter(task == i & GENDER == "female") %>%
+    filter(!is.na(probabilities)) %>%
+    group_by(model) %>%
+    roc_auc(truth_01, probabilities, event_level = 'second') %>%
+    rename(female = .estimate)
+  
+  auc_male = df1 %>%
+    filter(task == i & GENDER == "male") %>%
+    filter(!is.na(probabilities)) %>%
+    group_by(model) %>%
+    roc_auc(truth_01, probabilities, event_level = 'second') %>%
+    rename(male = .estimate)
+  
+  auc = full_join(auc, auc_female, by = c("model", ".metric", ".estimator"))
+  auc = full_join(auc, auc_male, by = c("model", ".metric", ".estimator"))
+  
+  measures = bind_rows(measures, auc)
+  
+  measures = mutate(measures, gender_diff = male - female)
+  
+  # order variables for nicer plotting
+  metrics_order <- c("accuracy", 
+                     "sens", # sensitivity, recall, TPR
+                     "spec", # specificity, TNR
+                     "roc_auc",
+                     "precision", # Precision, Positive predictive value PPV
+                     "f_meas" # fbeta, with beta = 1
+  )
+  measures$.metric = factor(measures$.metric, level = metrics_order)
+  
+  model_order <- c("Featureless", "OR", "Logistic Regression", "encode.colapply.classif.glmnet", "Random Forest",
+                   "Decision Tree", "encode.colapply.classif.xgboost", "encode.colapply.classif.svm", "KKNN"
+  )
+  measures$model = factor(measures$model, level = rev(model_order))
+  
+  # append to list over tasks
+  measure_list = append(measure_list, list(measures))
+}
+names(measure_list) = unique(df1$task)
 
-performance_measures = df1 %>%
-  filter(task == "AMS youth") %>%
-  group_by(model) %>%
-  performance_measure_set(truth_01, estimate = estimate_0.66, event_level = 'second')
 
-measures_female = df1 %>%
-  filter(task == "AMS youth" & GENDER == "female") %>%
-  group_by(model) %>%
-  performance_measure_set(truth_01, estimate = estimate_0.66, event_level = 'second') %>%
-  rename(female = .estimate)
-
-measures_male = df1 %>%
-  filter(task == "AMS youth" & GENDER == "male") %>%
-  group_by(model) %>%
-  performance_measure_set(truth_01, estimate = estimate_0.66, event_level = 'second') %>%
-  rename(male = .estimate)
-
-measures = full_join(performance_measures, measures_female, by = c("model", ".metric", ".estimator"))
-measures = full_join(measures, measures_male, by = c("model", ".metric", ".estimator"))
-
-# AUC
-auc = df1 %>%
-  filter(task == "AMS youth" & GENDER == "female") %>%
-  group_by(model) %>%
-  roc_auc(truth_01, probabilities, event_level = 'second')
-
-auc_female = df1 %>%
-  filter(task == "AMS youth" & GENDER == "female") %>%
-  group_by(model) %>%
-  roc_auc(truth_01, probabilities, event_level = 'second') %>%
-  rename(female = .estimate)
-
-auc_male = df1 %>%
-  filter(task == "AMS youth" & GENDER == "male") %>%
-  group_by(model) %>%
-  roc_auc(truth_01, probabilities, event_level = 'second') %>%
-  rename(male = .estimate)
-
-auc = full_join(auc, auc_female, by = c("model", ".metric", ".estimator"))
-auc = full_join(auc, auc_male, by = c("model", ".metric", ".estimator"))
-
-measures = bind_rows(measures, auc)
-
-
-# Heatmap
-heatmap(measures, model, .metric, .estimate)
-heatmap(measures, model, .metric, male)
-heatmap(measures, model, .metric, female)
+# Heatmap --------------------------------------------------------------------------------------------------------------
+heatmap_list = list()
+for(i in unique(df1$task)){
+  p = heatmap(measure_list[[i]], model, .metric, .estimate) +
+    ggtitle(i)
+  heatmap_list = append(heatmap_list,  list(p))
+}
+heatmap_male_list = list()
+for(i in unique(df1$task)){
+  p = heatmap(measure_list[[i]], model, .metric, male) +
+    ggtitle(i)
+  heatmap_male_list = append(heatmap_male_list,  list(p))
+}
+heatmap_female_list = list()
+for(i in unique(df1$task)){
+  p = heatmap(measure_list[[i]], model, .metric, female) +
+    ggtitle(i)
+  heatmap_female_list = append(heatmap_female_list,  list(p))
+}
+heatmap_genderdiff_list = list()
+for(i in unique(df1$task)){
+  p = heatmap_diff(measure_list[[i]], model, .metric, gender_diff) +
+    ggtitle(i)
+  heatmap_genderdiff_list = append(heatmap_genderdiff_list,  list(p))
+}
 
 
 # RESTE ################################################################################################################
@@ -145,3 +201,23 @@ subset(df3, subset = (task == "AMS youth" & model != "OR" ))
 
 autoplot(roc_curve(subset(df1, subset = (task == "AMS youth" & model == "Logistic Regression")), 
                    truth_01, probabilities, event_level = "second"))
+
+# ROC Curve
+subset(df1, subset = (task == "AMS youth")) %>%
+  #filter(model == "OR" | model == "Logistic Regression" | model == "encode.colapply.classif.glmnet") %>%
+  group_by(model) %>%
+  roc_curve(truth_01, probabilities, event_level = 'second') %>%
+  autoplot() +
+  ggtitle("AMS youth")
+
+
+for(i in unique(df1$task)){
+  subset(df1, subset = (task == i)) %>%
+    filter(!is.na(probabilities)) %>%
+    #filter(model == "OR" | model == "Logistic Regression" | model == "encode.colapply.classif.glmnet") %>%
+    group_by(model) %>%
+    roc_curve(truth_01, probabilities, event_level = 'second') %>%
+    autoplot() +
+    ggtitle(i)
+}
+
