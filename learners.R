5e1cf35c4b7a47ecdb3bb3338cc536306599d0c6
Viktoria Szabo <40633060+vik-sz@users.noreply.github.com>
2022-06-13 22:25:52 +0200

    Autotune models

--- learners.R
+++ learners.R
@@ -2,167 +2,189 @@
 library(mlr3verse) # machine learning set-up for modeling
 set.seed(42)
 
-# Preperation ----------------------------------------------------------------------------------------------------------
+# Preperation ==========================================================================================================
 # 3 fold cross validation for inner loop
 resampling_inner_3CV = rsmp("cv", folds = 3L)
 
 measures_tuning = msr("classif.auc")
 
-# Learners -------------------------------------------------------------------------------------------------------------
-# Penalized logistic regression glmnet _________________________________________________________________________________
+# Choose optimization algorithm:
+# no need to etra randomize, try to go every step
+#tuner_grid_search_ranger = tnr("grid_search", resolution = 5)  
+tuner = tnr("random_search")
+
+# Set the budget (when to terminate):
+terminator = trm("evals", n_evals = 5)
+
+
+# use mlr3tuningspaces for default tuning search spaces
+# Collection of search spaces for hyperparameter tuning. 
+# Includes various search spaces that can be directly applied to an mlr3 learner. 
+# Additionally, meta information about the search space can be queried.
+as.data.table(mlr_tuning_spaces)
+
+# Tuning spaces from the Bischl (2021) article.
+
+# Learners =============================================================================================================
+# glmnet: Penalized logistic regression --------------------------------------------------------------------------------
 learner_glmnet = lrn("classif.glmnet", predict_type = "prob", predict_sets = "test")
+
+tuning_space_glmnet = lts("classif.glmnet.rbv2")
+learner_glmnet$param_set$values = tuning_space_glmnet$values
+
+learner_glmnet$param_set$values
+
+tuner_glmnet = AutoTuner$new(
+  learner = learner_glmnet,
+  resampling = resampling_inner_3CV,
+  measure = measures_tuning, 
+  #search_space = param_ranger, 
+  terminator = terminator,
+  tuner = tuner
+)
+
 fencoder = po("encode", method = "treatment", affect_columns = selector_type("factor"))
 ord_to_int = po("colapply", applicator = as.integer, affect_columns = selector_type("ordered"))
-graph = fencoder %>>% ord_to_int %>>% learner_glmnet
+graph = fencoder %>>% ord_to_int %>>% tuner_glmnet
 
 graph_glmnet = as_learner(graph)
 
-# xgboost ______________________________________________________________________________________________________________
-# Define learner:
-learner_xgboost = lrn("classif.xgboost", predict_type = "prob", predict_sets = "test")
-graph = fencoder %>>% ord_to_int %>>% learner_xgboost
+# Tune lambda
+# https://mlr3learners.mlr-org.com/reference/mlr_learners_classif.glmnet.html 
 
-graph_xgboost = as_learner(graph)
+# Decision Tree --------------------------------------------------------------------------------------------------------
+learner_rpart = lrn("classif.rpart", id = "Decision Tree", predict_type = "prob", predict_sets = "test")
+learner_rpart$param_set
 
-# tune nrounds, eta and booster:
-learner_xgboost$param_set
+tuning_space_rpart = lts("classif.rpart.default")
+learner_rpart$param_set$values = tuning_space_rpart$values
 
-# Hyperparameter subset of XGBoost
-param_xgboost = ps(
-  nrounds = p_int(lower = 1, upper = 16, tags = "budget"),
-  eta = p_dbl(lower = 0, upper = 1),
-  booster = p_fct(levels = c("gbtree", "gblinear", "dart"))
-)
-
-# Choose optimization algorithm:
-# no need to etra randomize, try to go every step
-tuner_grid_search_ranger = tnr("grid_search", resolution = 30)  
-
-# Set the budget (when to terminate):
-terminator_xgboost = trm("evals", n_evals = 30)
-
-# Set up autotuner instance with the predefined setups
-tuner_ranger = AutoTuner$new(
-  learner = learner_xgboost,
-  resampling = resampling_inner_3CV,
-  measure = measures_tuning, 
-  search_space = param_xgboost, 
-  terminator = terminator_xgboost,
-  tuner = tuner_grid_search_ranger
-)
-
-
-# SVM __________________________________________________________________________________________________________________
-learner_svm = lrn("classif.svm", predict_type = "prob", predict_sets = "test")
-graph = fencoder %>>% ord_to_int %>>% learner_svm
-
-graph_svm = as_learner(graph)
-
-# tune mtry for the random forest:
-learner_svm$param_set
-
-search_space = ps(cost = p_dbl(0.1, 10), kernel = p_fct(c("polynomial", "radial")))
-# see searchspace grid
-# rbindlist(generate_design_grid(search_space, 3)$transpose())
-
-# The Support Vector Machine (SVM), for example, has the degree parameter that is only valid when kernel is "polynomial"
-# search_space = ps(
-#   cost = p_dbl(-1, 1, trafo = function(x) 10^x),
-#   kernel = p_fct(c("polynomial", "radial")),
-#   degree = p_int(1, 3, depends = kernel == "polynomial")
-# )
-# rbindlist(generate_design_grid(search_space, 3)$transpose(), fill = TRUE)
-
-
-# we will try all configurations: 1 to 21 features.
-param_mtry = ps(mtry = p_int(lower = 1, upper = 30)) 
-
-# Choose optimization algorithm:
-# no need to etra randomize, try to go every step
-tuner_grid_search_ranger = tnr("grid_search", resolution = 30)  
-
-# Set the budget (when to terminate):
-terminator_mtry = trm("evals", n_evals = 30)
-
-# Set up autotuner instance with the predefined setups
-tuner_ranger = AutoTuner$new(
-  learner = learner_svm,
-  resampling = resampling_inner_3CV,
-  measure = measures_tuning, 
-  search_space = param_mtry, 
-  terminator = terminator_mtry,
-  tuner = tuner_grid_search_ranger
-)
-
-
-# Regression Tree ______________________________________________________________________________________________________
 # The complexity hyperparameter cp that controls when the learner considers introducing another branch.
 # The minsplit hyperparameter that controls how many observations must be present in a leaf for another split to be attempted.
 
-search_space = ps(
-  cp = p_dbl(lower = 0.001, upper = 0.1),
-  minsplit = p_int(lower = 1, upper = 10)
-)
-
-learner_tree = lrn("classif.rpart",
-                   predict_type = "prob",
-                   "cp" = 0.001) 
-# set cp super low to enforce new splits so we get FPR < 1%
-
-# Autotune knn _________________________________________________________________________________________________________
-# Define learner:
-learner_knn = lrn("classif.kknn", predict_type = "prob", predict_sets = "test")
-
-# tune k in knn:
-learner_knn$param_set
-
-# tune the chosen hyperparameters with these boundaries:
-param_k = ps(k = p_int(lower = 1, upper = 50))
-
-# Choose optimization algorithm:
-# no need to randomize, try to go every step
-tuner_grid_search_knn = tnr("grid_search", resolution = 50)
-
-# Set the budget (when to terminate):
-# test every candidate
-terminator_knn = trm("evals", n_evals = 50)
-
-# Set up autotuner instance with the predefined setups
-tuner_knn = AutoTuner$new(
-  learner = learner_knn,
+tuner_rpart = AutoTuner$new(
+  learner = learner_rpart,
   resampling = resampling_inner_3CV,
   measure = measures_tuning, 
-  search_space = param_k, 
-  terminator = terminator_knn,
-  tuner = tuner_grid_search_knn
+  #search_space = param_ranger, 
+  terminator = terminator,
+  tuner = tuner
 )
 
-# Autotune Random Forest _______________________________________________________________________________________________
-# Define learner:
-learner_ranger = lrn("classif.ranger", predict_type = "prob", predict_sets = "test") #, importance = "impurity")
 
-# tune mtry for the random forest:
+# Random Forest --------------------------------------------------------------------------------------------------------
+# Define learner:
+learner_ranger = lrn("classif.ranger", id = "Random Forest", predict_type = "prob", predict_sets = "test")
+
+# possible parameter to tune:
 learner_ranger$param_set
 
-# we will try all configurations: 1 to 21 features.
-param_mtry = ps(mtry = p_int(lower = 1, upper = 30)) 
+# get tuning space from default tuning search spaces
+tuning_space_ranger = lts("classif.ranger.default")
 
-# Choose optimization algorithm:
-# no need to etra randomize, try to go every step
-tuner_grid_search_ranger = tnr("grid_search", resolution = 30)  
+learner_ranger$param_set$values = tuning_space_ranger$values
 
-# Set the budget (when to terminate):
-terminator_mtry = trm("evals", n_evals = 30)
 
 # Set up autotuner instance with the predefined setups
 tuner_ranger = AutoTuner$new(
   learner = learner_ranger,
   resampling = resampling_inner_3CV,
   measure = measures_tuning, 
-  search_space = param_mtry, 
-  terminator = terminator_mtry,
-  tuner = tuner_grid_search_ranger
+  #search_space = param_ranger, 
+  terminator = terminator,
+  tuner = tuner
+)
+#tuner_ranger$train(task_ams)
+
+# xgboost --------------------------------------------------------------------------------------------------------------
+# Define learner:
+learner_xgboost = lrn("classif.xgboost", predict_type = "prob", predict_sets = "test")
+
+# tune nrounds, eta and booster:
+learner_xgboost$param_set
+
+# Parameters to tune according to mlr3 website:
+# nrounds
+
+# get tuning space from default tuning search spaces
+tuning_space_xgboost = lts("classif.xgboost.default")
+
+learner_xgboost$param_set$values = tuning_space_xgboost$values
+tuning_space_xgboost$values
+
+
+# Set up autotuner instance with the predefined setups
+tuner_xgboost = AutoTuner$new(
+  learner = learner_xgboost,
+  resampling = resampling_inner_3CV,
+  measure = measures_tuning, 
+  terminator = terminator,
+  tuner = tuner
+)
+
+graph = fencoder %>>% ord_to_int %>>% tuner_xgboost
+graph_xgboost = as_learner(graph)
+
+# SVM ------------------------------------------------------------------------------------------------------------------
+learner_svm = lrn("classif.svm", predict_type = "prob", predict_sets = "test")
+
+# Possible tuning parameter
+learner_svm$param_set
+
+# get tuning space from default tuning search spaces
+tuning_space_svm = lts("classif.svm.rbv2")
+
+learner_svm$param_set$values = tuning_space_svm$values
+tuning_space_svm$values
+
+
+# Set up autotuner instance with the predefined setups
+tuner_svm = AutoTuner$new(
+  learner = learner_svm,
+  resampling = resampling_inner_3CV,
+  measure = measures_tuning, 
+  terminator = terminator,
+  tuner = tuner
+)
+
+graph = fencoder %>>% ord_to_int %>>% tuner_svm
+graph_svm = as_learner(graph)
+
+
+# Error in self$assert(xs) : 
+#   Assertion on 'xs' failed: The parameter 'cost' can only be set if the following condition is met 'type = C-classification'. Instead the parameter value for 'type' is not set at all. Try setting 'type' to a value that satisfies the condition.
+# This happened PipeOp classif.svm.tuned's $train()
+
+# Autotune kknn --------------------------------------------------------------------------------------------------------
+# Define learner:
+learner_kknn = lrn("classif.kknn", predict_type = "prob", predict_sets = "test")
+
+# tune k in knn:
+learner_kknn$param_set
+
+# get tuning space from default tuning search spaces
+tuning_space_kknn = lts("classif.kknn.rbv2")
+
+learner_kknn$param_set$values = tuning_space_kknn$values
+tuning_space_kknn$values
+
+
+# Set up autotuner instance with the predefined setups
+tuner_kknn = AutoTuner$new(
+  learner = learner_kknn,
+  resampling = resampling_inner_3CV,
+  measure = measures_tuning, 
+  terminator = terminator,
+  tuner = tuner
 )
 
 
+# resampling_outer_ho = rsmps("holdout", ratio = 0.8)
+# design = benchmark_grid(
+#   tasks = task_ams,
+#   learners = c(tuner_rpart, tuner_ranger, graph_glmnet, graph_xgboost, tuner_kknn),
+#   resamplings = resampling_outer_ho
+# )
+# bmr = benchmark(design)
+# bmr$score(performance_measures)
 
